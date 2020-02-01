9247016b84356894ad4652a0c409916951a80570
Charles T. Gray <charlestigray@gmail.com>
2020-02-01 12:24:20 +1100

    :snake:

--- tests/testthat/test-neet.R
+++ tests/testthat/test-neet.R
@@ -1,11 +1,6 @@
 context("non-empty thing of expected type")
 
-expect_neet <- function(fn_output, output_class) {
-  expect_is(fn_output, output_class)
-  expect_false(is.na(fn_output))
-  expect_false(is.null(fn_output))
-  expect_false(fn_output == Inf | fn_output == -Inf)
-}
+library(neet)
 
 test_that("g_*", {
   expect_neet(g_exp(3, 5), "numeric")
@@ -18,37 +13,45 @@ test_that("effect_se", {
   # testing sample
   a_sample <- rexp(100, 3)
 
-  expect_neet(effect_se(
-    centre = mean(a_sample),
-    spread = sd(a_sample),
-    n = length(a_sample),
-    centre_type = "mean",
-    spread_type = "sd"
-  ), "numeric")
+  expect_neet(
+    effect_se(
+      centre = mean(a_sample),
+      spread = sd(a_sample),
+      n = length(a_sample),
+      centre_type = "mean",
+      spread_type = "sd"
+    ), "numeric"
+  )
 
-  expect_neet(effect_se(
-    centre = mean(a_sample),
-    spread = var(a_sample),
-    n = length(a_sample),
-    centre_type = "mean",
-    spread_type = "var"
-  ), "numeric")
+  expect_neet(
+    effect_se(
+      centre = mean(a_sample),
+      spread = var(a_sample),
+      n = length(a_sample),
+      centre_type = "mean",
+      spread_type = "var"
+    ), "numeric"
+  )
 
-  expect_neet(effect_se(
-    centre = median(a_sample),
-    spread = IQR(a_sample),
-    n = length(a_sample),
-    centre_type = "median",
-    spread_type = "iqr"
-  ), "numeric")
+  expect_neet(
+    effect_se(
+      centre = median(a_sample),
+      spread = IQR(a_sample),
+      n = length(a_sample),
+      centre_type = "median",
+      spread_type = "iqr"
+    ), "numeric"
+  )
 
-  expect_neet(effect_se(
-    centre = median(a_sample),
-    spread = diff(range(a_sample)),
-    n = length(a_sample),
-    centre_type = "median",
-    spread_type = "range"
-  ), "numeric")
+  expect_neet(
+    effect_se(
+      centre = median(a_sample),
+      spread = diff(range(a_sample)),
+      n = length(a_sample),
+      centre_type = "median",
+      spread_type = "range"
+    ), "numeric"
+  )
 
 })
 
@@ -62,8 +65,7 @@ test_that("other estimators", {
     median(a_sample),
     quantile(a_sample, 0.75),
     max(a_sample)
-  ),
-  "numeric")
+  ), "numeric")
 
   expect_neet(bland_se(
     min(a_sample),
@@ -72,8 +74,7 @@ test_that("other estimators", {
     quantile(a_sample, 0.75),
     max(a_sample),
     length(a_sample)
-  ),
-  "numeric")
+  ), "numeric")
 
   expect_neet(hozo_mean(min(a_sample),
                         median(a_sample),
@@ -100,8 +101,7 @@ test_that("other estimators", {
     median(a_sample),
     quantile(a_sample, 0.75),
     max(a_sample)
-  ),
-  "numeric")
+  ), "numeric")
 
   expect_neet(wan_se_C2(
     min(a_sample),
@@ -110,8 +110,7 @@ test_that("other estimators", {
     quantile(a_sample, 0.75),
     max(a_sample),
     length(a_sample)
-  ),
-  "numeric")
+  ), "numeric")
 
   expect_neet(wan_mean_C3(
     quantile(a_sample, 0.25),
