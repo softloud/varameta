abab30182d5ec8cc3283f41c65d248d9c8572915
Charles T. Gray <charlestigray@gmail.com>
2020-03-08 10:41:16 +1100

    #7 made a start on restructuring

--- R/bland_mean.R
+++ R/bland_mean.R
@@ -1,18 +1,49 @@
-#' Estimate Mean from Quartiles and Range
+#' Estimate Mean from Quartiles and Range (Bland)
 #'
-#' See \code{\link{estimator_eqn_ref}} for details and references for equation
-#' (10).
+#' Method provided by Bland [todo: citation].
 #'
 #' @param a Minimum value of sample.
 #' @param q_1 First quartile of sample.
 #' @param m Median of sample.
 #' @param q_3 Third quartile of sample.
 #' @param b Maximum value of sample.
+#' @param n Sample size.
 #'
-#' @family mean_estimator
+#' @family one_neet Inputs and outputs neet tested.
 #'
+#' @name bland
+NULL
+#> NULL
+
+#' @rdname bland
+#' @family mean_estimator Approximate an estimator for the mean from quartiles.
 #' @export
 
 bland_mean <- function(a, q_1, m, q_3, b) {
+  neet::assert_neet(a, "numeric")
+  neet::assert_neet(q_1, "numeric")
+  neet::assert_neet(m, "numeric")
+  neet::assert_neet(q_3, "numeric")
+  neet::assert_neet(b, "numeric")
+
   (a + 2 * q_1 + 2 * m + 2 * q_3 + b) / 8
+}
+
+#' @rdname bland
+#' @family mean_estimator Approximate an estimator for the sd from quartiles.
+#' @export
+
+bland_se <- function(a, q_1, m, q_3, b, n) {
+  neet::assert_neet(a, "numeric")
+  neet::assert_neet(q_1, "numeric")
+  neet::assert_neet(m, "numeric")
+  neet::assert_neet(q_3, "numeric")
+  neet::assert_neet(b, "numeric")
+  neet::assert_neet(n, "numeric")
+
+  sqrt(
+    (1 / 16 * (a ^ 2 + 2 * q_1 ^ 2 + 2 * m ^ 2 + 2 * q_3 ^ 2 + b ^ 2) +
+       1 / 8 * (a * q_1 + q_1 * m + m * q_3 + q_3 * b) -
+       1 / 64 * (a + 2 * q_1 + 2 * m + 2 * q_3 + b) ^ 2
+    ) / n)
 }
