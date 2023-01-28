6fd59a6fb33977860e4493bf35baa1638a07d564
Charles T. Gray <charlestigray@gmail.com>
2023-01-28 18:13:07 +0100

    not sure if useful change

---
+++ R/wan.R
@@ -0,0 +1,5 @@
+#' Estimating the sample mean and standard deviation
+#'
+#' Using [Wan et al.](https://bmcmedresmethodol.biomedcentral.com/articles/10.1186/1471-2288-14-135/tables/3)'s method.
+#'
+#' @export
